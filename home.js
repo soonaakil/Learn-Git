c1faafb49731e3d45b10f9a29fdf984b36d0d2a5
Piddi <piddi@gmail.com>
2024-11-17 16:25:46 +0530

    adding home.js

---
+++ home.js
@@ -0,0 +1,9 @@
+function addN(){
+    let x = 10;
+    let y = 9;
+    let add = x + y
+    // console.log(add);
+    return "How to calculate (X + Y): "+add;
+}
+document.write(addN());
+console.log(addN());
